901e394cf9e2a1044ec4f43af686ad0d9a912b53
archways <107893165+archways404@users.noreply.github.com>
2024-01-09 14:26:28 +0100

    testing

--- project_files/src/renderer/src/App.jsx
+++ project_files/src/renderer/src/App.jsx
@@ -3,12 +3,12 @@ import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 import { useDrag } from 'react-dnd'
 
-import test from './components/test'
+import Test from './components/test'
 
 function App() {
   return (
     <DndProvider backend={HTML5Backend}>
-      <test />
+      <Test />
     </DndProvider>
   )
 }

4ce3f826b75f72c64b8662ece0f54e9d898daf2a
archways <107893165+archways404@users.noreply.github.com>
2024-01-09 15:37:23 +0100

    link

--- project_files/src/renderer/src/main.jsx
+++ project_files/src/renderer/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import './assets/index.css'
 import Layout from './Layout'
 
+// https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Layout />

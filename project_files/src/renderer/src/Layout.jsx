34d65b1c82d883a53293fe42fc6894b69ac1b3fb
archways <107893165+archways404@users.noreply.github.com>
2024-01-17 08:48:09 +0100

    init

--- project_files/src/renderer/src/Layout.jsx
+++ project_files/src/renderer/src/Layout.jsx
@@ -1,43 +1,91 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react'
-import Markdownrender from './components/Markdownrender'
-import Popup from './components/Popup'
+import { DndProvider, useDrag, useDrop } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import 'tailwindcss/tailwind.css'
+import Markdownrender from './components/Markdownrender' // Import your Markdown render component
+
+const initialItems = [
+  { id: 'item-1', content: 'Item 1' },
+  { id: 'item-2', content: 'Item 2' },
+  { id: 'item-3', content: 'Item 3' }
+]
+
+const DraggableItem = ({ item, itemType }) => {
+  const [{ isDragging }, dragRef] = useDrag(() => ({
+    type: itemType,
+    item: { id: item.id, type: itemType, content: item.content },
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging()
+    })
+  }))
+
+  return (
+    <div
+      ref={dragRef}
+      className={`p-2 mb-2 bg-white border rounded shadow-sm ${isDragging ? 'opacity-50' : ''}`}
+    >
+      {item.content}
+    </div>
+  )
+}
+
+const DroppableArea = ({ onDropItem, items, itemType, className }) => {
+  const [, dropRef] = useDrop({
+    accept: itemType === 'item' ? 'droppedItem' : 'item',
+    drop: (item, monitor) => {
+      onDropItem(item, itemType)
+    }
+  })
+
+  return (
+    <div ref={dropRef} className={className}>
+      {items.map((item) => (
+        <DraggableItem key={item.id} item={item} itemType={itemType} />
+      ))}
+    </div>
+  )
+}
 
 function Layout() {
-  //const [markdown, setMarkdown] = useState('')
-  const [markdown, setMarkdown] = useState('## This is Markdown\nThis is a test.')
-  const [activePopup, setActivePopup] = useState(null)
+  const [leftItems, setLeftItems] = useState(initialItems)
+  const [rightItems, setRightItems] = useState([])
+  const [markdownContent, setMarkdownContent] = useState('')
 
-  const openPopup = (type) => {
-    setActivePopup(type)
-  }
-
-  const closePopup = () => {
-    setActivePopup(null)
+  const handleDrop = (droppedItem, targetType) => {
+    if (targetType === 'item') {
+      // Move item from right to left
+      setRightItems((prevItems) => prevItems.filter((item) => item.id !== droppedItem.id))
+      setLeftItems((prevLeft) => [...prevLeft, droppedItem])
+    } else {
+      // Move item from left to right
+      setLeftItems((prevItems) => prevItems.filter((item) => item.id !== droppedItem.id))
+      setRightItems((prevRight) => [...prevRight, droppedItem])
+      setMarkdownContent(droppedItem.content) // Update Markdown view
+    }
   }
 
   return (
-    <div className="layout">
-      <div className="button-container">
-        <button onClick={() => openPopup('codeblock')}>CodeblockBtn</button>
-        <button onClick={() => openPopup('blockquote')}>BlockquoteBtn</button>
-        <button onClick={() => openPopup('bold')}>BoldBtn</button>
-        <button onClick={() => openPopup('italics')}>ItalicsBtn</button>
-        <button onClick={() => openPopup('link')}>LinkBtn</button>
-        <button onClick={() => openPopup('image')}>ImageBtn</button>
-        <button onClick={() => openPopup('code')}>CodeBtn</button>
-        <button onClick={() => openPopup('header')}>HeaderBtn</button>
-        <button onClick={() => openPopup('list')}>ListBtn</button>
-        <button onClick={() => openPopup('tasklist')}>TasklistBtn</button>
-        <button onClick={() => openPopup('table')}>TableBtn</button>
-        <button onClick={() => openPopup('footnote')}>FootnoteBtn</button>
-        <button onClick={() => openPopup('emoji')}>EmojiBtn</button>
+    <DndProvider backend={HTML5Backend}>
+      <div className="flex h-screen">
+        <DroppableArea
+          onDropItem={handleDrop}
+          items={leftItems}
+          itemType="item"
+          className="w-1/5 bg-gray-100 p-4"
+        />
+        <DroppableArea
+          onDropItem={handleDrop}
+          items={rightItems}
+          itemType="droppedItem"
+          className="w-1/5 bg-gray-200 p-4"
+        />
+        <div className="w-3/5 bg-gray-300 p-4">
+          <Markdownrender markdown={markdownContent} />
+        </div>
       </div>
-      <div className="markdown-container">
-        <Markdownrender markdown={markdown} />
-      </div>
-      {activePopup && <Popup type={activePopup} onClose={closePopup} />}
-    </div>
+    </DndProvider>
   )
 }
 
